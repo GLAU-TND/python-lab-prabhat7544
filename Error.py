1dc225a23d2262a7afbc76b8c40613d37b916ee8
prabhat kumar <prabhat.baluaa@gmail.com>
2019-11-28 17:00:08 +0530

    solution of Qno 2

--- Error.py
+++ Error.py
@@ -1,6 +1,22 @@
-a = []
-a.addvalue(4)
-try:
-    AttributeError
-except AttributeError:
-    print("Attribute error occured")
+class MyException(Exception):
+    def __init__(self, v):
+        self.v = v
+
+
+def exceptionhandling():
+    a = 10
+    try:
+        a += 'abc'
+    except TypeError as e:
+        print(type(e).__name__, e)
+    try:
+        a.abcd()
+    except AttributeError as e:
+        print(type(e).__name__, e)
+    try:
+        int('sd')
+    except ValueError as e:
+        print(type(e).__name__, e)
+
+
+exceptionhandling()
